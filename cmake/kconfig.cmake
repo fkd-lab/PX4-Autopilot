9e25db8bcdb281d2e122c6134d842f18f681bf64
hamataku <hamataku07130713@gmail.com>
2026-01-05 14:02:25 +0900

    exclude CONFIG_BOARD_CONSTRAINED_FLASH

--- cmake/kconfig.cmake
+++ cmake/kconfig.cmake
@@ -334,6 +334,8 @@ if(EXISTS ${BOARD_DEFCONFIG})
 	if(CONSTRAINED_FLASH)
 		set(px4_constrained_flash_build "1" CACHE INTERNAL "constrained flash build" FORCE)
 		add_definitions(-DCONSTRAINED_FLASH)
+	else()
+		set(px4_constrained_flash_build "0" CACHE INTERNAL "constrained flash build" FORCE)
 	endif()
 
 	if(NO_HELP)
